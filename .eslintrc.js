3b3c07ba7709d9e3392ad3b28c00e50dee785d45
Ajil <ajilpaul.268@gmail.com>
2025-02-19 21:37:45 +0530

    Added eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,43 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
-    "plugin:storybook/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
   ],
-  plugins: ["@typescript-eslint", "react", "prettier"],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
   env: {
     browser: true,
     node: true,
     jest: true,
+    es6: true,
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {},
     },
   },
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
   },
 };

153a1fa7c26361078a16600e4694752be69bb904
Ajil <ajilpaul.268@gmail.com>
2025-03-11 14:42:56 +0530

    Added alert component

---
+++ src/components/alert/alert.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@base/alert-dialog';
+import { Button } from '@components/button';
+
+import { AlertProps } from './types';
+
+const Alert: React.FC<AlertProps> = ({
+  isOpen = false,
+  title,
+  description,
+  actionLabel = 'Continue',
+  cancelLabel = 'Cancel',
+  triggerButtonProps,
+  onAction = () => {},
+  onClose = () => {},
+}) => (
+  <AlertDialog
+    {...(!triggerButtonProps && { open: isOpen })}
+    onOpenChange={(isOpen) => !isOpen && onClose()}
+  >
+    {triggerButtonProps && (
+      <AlertDialogTrigger asChild>
+        <Button {...triggerButtonProps} />
+      </AlertDialogTrigger>
+    )}
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>{title}</AlertDialogTitle>
+        <AlertDialogDescription>{description}</AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>{cancelLabel}</AlertDialogCancel>
+        <Button variant="destructive" onClick={onAction} label={actionLabel} />
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+);
+
+Alert.displayName = 'Alert';
+
+export { Alert };

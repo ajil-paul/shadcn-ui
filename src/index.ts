cbca64ed6cb2201c21e98723e6f44f656af0daa4
Ajil <ajilpaul.268@gmail.com>
2025-04-18 21:43:08 +0530

    Included missed components in build

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 export * from './components/alert';
+export * from './components/breadcrumb';
 export * from './components/button';
 export * from './components/checkbox';
+export * from './components/dropdown';
 export * from './components/input';
 export * from './components/label';
 export * from './components/radio-group';
 export * from './components/select';
+export * from './components/sidebar';
 export * from './components/switch';
+export * from './components/table';
 export * from './components/textarea';
 export * from './components/typography';
-export * from './components/dropdown';
-export * from './components/sidebar';
 
 export * from './components/form';
 
